cc9c26d6ec484d641fc4998ca8ba6468f314ef4e
mastatricky <luiz.costa@phonetrack.com.br>
2023-11-02 11:48:13 -0300

    [feature] - added Mocha and Chai packages for testing, create Movie test which dispatch a request and execute their test. [change] - added more information to README.md regarding to test and run the project

--- README.md
+++ README.md
@@ -7,6 +7,22 @@
 - NPX 10.2.2
  
 2 º - Segundo passo, renomeie o arquivo "sample_db.sqlite" que está dentro do diretório "database", para "database.sqlite"
+
 3 º - Terceiro passo, execute o comando "npm install" para instalar o projeto e seus pacotes
+
 4 º - Quarto passo, executar o comando "npx sequeliza-cli db:migrate"
+
 5 º - Quinto passo, executar o comando "npx sequelize-cli db:seed:all"
+
+6 º - Sexto passo, executar o comando "npm start" para ele rodar o servidor
+
+7 º (Opcional) - Sétimo passo opcional, caso você tenha algum servidor TCP sendo rodado na porta padrão dessa aplicação(7548), você pode alterar a porta no arquivo "config/default.json" e rodar novamente o comando "npm start"
+
+8 º - Oitavo passo, você pode testar a requisição abaixo tanto no navegador, quanto via algum REST Client(Postman, SoapUI, Curl, etc)
+
+API:
+http://localhost:7548/awards/winners
+
+# Testes
+
+Para executar os testes, execute o comando "npm run test" os testes implementados foram feitos utilizando o "Mocha" e o "Chai"
---
+++ test/Movie.js
@@ -0,0 +1,111 @@
+const expect = require("chai").expect;
+const request = require("request");
+const config = require('config');
+const url = `http://localhost:${config.get('webserver.port')}`;
+
+
+describe("get /awards/winners", () => {
+    let successData = {
+        "min": [
+            {
+                "producer": "Joel Silver",
+                "interval": 1,
+                "previousWin": 1990,
+                "followingWin": 1991
+            }
+        ],
+        "max": [
+            {
+                "producer": "Matthew Vaughn",
+                "interval": 13,
+                "previousWin": 2002,
+                "followingWin": 2015
+            }
+        ]
+    };
+    it("return status 200", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            expect(resp.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it("check 'responseData.min' is an array", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.min).to.instanceOf(Array);
+            done();
+        });
+    });
+    it("check 'responseData.max' is an array", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.max).to.instanceOf(Array);
+            done();
+        });
+    });
+    it("check 'responseData.min[0].interval' is the small or equals to 'responseData.max[0].interval'", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.min[0].interval).to.lessThanOrEqual(successData.max[0].interval);
+            done();
+        });
+    });
+    // MIN index, test
+    it("check 'responseData.min[0].producer' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.min[0].producer).to.equal(successData.min[0].producer);
+            done();
+        });
+    });
+    it("check 'responseData.min[0].interval' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.min[0].interval).to.equal(successData.min[0].interval);
+            done();
+        });
+    });
+    it("check 'responseData.min[0].previousWin' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.min[0].previousWin).to.equal(successData.min[0].previousWin);
+            done();
+        });
+    });
+    it("check 'responseData.min[0].followingWin' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.min[0].followingWin).to.equal(successData.min[0].followingWin);
+            done();
+        });
+    });
+    // MAX index, test
+    it("check 'responseData.max[0].producer' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.max[0].producer).to.equal(successData.max[0].producer);
+            done();
+        });
+    });
+    it("check 'responseData.max[0].interval' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.max[0].interval).to.equal(successData.max[0].interval);
+            done();
+        });
+    });
+    it("check 'responseData.max[0].previousWin' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.max[0].previousWin).to.equal(successData.max[0].previousWin);
+            done();
+        });
+    });
+    it("check 'responseData.max[0].followingWin' is the same as our sample", (done) => {
+        request(url+"/awards/winners", (err, resp, body) => {
+            const responseData = JSON.parse(body);
+            expect(responseData.max[0].followingWin).to.equal(successData.max[0].followingWin);
+            done();
+        });
+    });
+});
